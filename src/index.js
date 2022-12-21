b5d8a8ca0e300310e776309619c4e47c6598725f
= <yemunRussom10@gmail.com>
2022-12-21 16:26:52 +0000

    create cohort done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 const cohortList = []
 
-// function create(description) {
-//     const todo = {
-//         id: todoList.length + 1,
-//         description,
-//         status: 'incomplete'
-//     }
-
-//     todoList.push(todo)
-
-//     return todo
-// }
 function create(name) {
   const cohort = {
     id: cohortList.length + 1,
