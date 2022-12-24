00fa6de4249a3424114f0707d120836d39c8c192
= <yemunRussom10@gmail.com>
2022-12-24 22:20:12 +0000

    Standard requirenment done

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,8 +1,58 @@
-const { create, resetCohort, search } = require('../src/index.js')
+const {
+  create,
+  resetCohort,
+  search,
+  addStudent,
+  removeCohort,
+  removeStudent
+} = require('../src/index.js')
 
 describe('Cohort Manger', () => {
   beforeEach(() => {
     resetCohort()
+  })
+  it('should remove a student', () => {
+    addStudent('cohort1', 5678, 'Bob', 'Nicolas', 'bob@github', 'bob@gmail')
+    addStudent('cohort2', 1234, 'John', 'Smith', 'john@github', 'john@gmail')
+    const student1 = removeStudent(1234)
+
+    expect(student1).toEqual({
+      cohort: 'cohort2',
+      studentId: 1234,
+      firstName: 'John',
+      lastname: 'Smith',
+      githubUserName: 'john@github',
+      email: 'john@gmail'
+    })
+  })
+
+  it('should remove a cohort', () => {
+    create('cohort1')
+    create('cohort2')
+    const cohort1 = removeCohort('cohort1')
+    expect(cohort1).toEqual({
+      id: 1,
+      name: 'cohort1'
+    })
+  })
+  it('should add a student', () => {
+    const student1 = addStudent(
+      'cohort2',
+      1234,
+      'John',
+      'Smith',
+      'john@github',
+      'yemun@gmail'
+    )
+
+    expect(student1).toEqual({
+      cohort: 'cohort2',
+      studentId: 1234,
+      firstName: 'John',
+      lastname: 'Smith',
+      githubUserName: 'john@github',
+      email: 'yemun@gmail'
+    })
   })
 
   it('should search a cohort', () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 const cohortList = []
+const studentList = []
 
+// Remove student from a specific cohort
+function removeStudent(id) {
+  const found = studentList.find((student) => student.studentId === id)
+
+  if (!found) {
+    return false
+  }
+  const index = studentList.findIndex((s) => s.studentId === id)
+  studentList.slice(index, 1)
+  return found
+}
+// Remove a cohort by cohort name
+function removeCohort(name) {
+  const found = cohortList.find((cohort) => cohort.name === name)
+
+  if (!found) {
+    return false
+  }
+  const index = cohortList.findIndex((c) => c.name === name)
+  cohortList.slice(index, 1)
+  return found
+}
+// Add student to a specific cohort
+function addStudent(
+  cohort,
+  studentId,
+  firstName,
+  lastname,
+  githubUserName,
+  email
+) {
+  const student = {
+    cohort,
+    studentId,
+    firstName,
+    lastname,
+    githubUserName,
+    email
+  }
+  studentList.push(student)
+  return student
+}
+
+// Search for a cohort by cohort name
 function search(name) {
   const found = cohortList.find((cohort) => cohort.name === name)
   if (found) {
@@ -7,6 +52,7 @@ function search(name) {
   }
 }
 
+// Create a cohort with a cohort name
 function create(name) {
   const cohort = {
     id: cohortList.length + 1,
@@ -17,6 +63,7 @@ function create(name) {
   return cohort
 }
 
+// reset cohort list so that it will every test will start from scratch.
 function resetCohort() {
   cohortList.splice(0, cohortList.length)
 }
@@ -24,5 +71,8 @@ function resetCohort() {
 module.exports = {
   create,
   resetCohort,
-  search
+  search,
+  addStudent,
+  removeCohort,
+  removeStudent
 }
